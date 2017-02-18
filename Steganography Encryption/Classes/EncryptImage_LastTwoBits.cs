f4dba5b666b75f6964bfed7dd3d60a0b2704781e
ursac <ursac@ADRIAN-SPACESTA>
2017-02-18 17:21:26 +0200

    Add project files.

---
+++ Steganography Encryption/Classes/EncryptImage_LastTwoBits.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steganography_Encryption.Classes
+{
+    class EncryptImage_LastTwoBits
+    {
+        private Bitmap Image;
+        private bool ColoredImage;
+        private int ElementLine = 0;
+        private int ElementColumn = 0;
+        private int DataSpace = 0;
+        private string Signature = "E69a44F";
+        private string Marker = "<_M>";
+
+        public EncryptImage_LastTwoBits(ref Bitmap ImageToEncrypt, bool ColoredImage)
+        {
+            this.Image = ImageToEncrypt;
+            this.ColoredImage = ColoredImage;
+            this.DataSpace = ImageToEncrypt.Height * ImageToEncrypt.Width;
+            if (ColoredImage)
+            {
+                this.DataSpace *= 3;
+            }
+        }
+
+        public bool EncryptText(string TextToEncrypt, string Code)
+        {
+            int AddSignature = 0;
+            string ConvertedMessage = "";
+            if (ElementLine == 0 && ElementColumn == 0)
+            {
+                AddSignature = Signature.Length * 4; // 4 = Pixels to encrypt one character
+                ConvertedMessage += Signature;
+            }
+            if ((TextToEncrypt.Length + Marker.Length * 2) * 4 + AddSignature > DataSpace)
+            {
+                return false;
+            }
+
+            DataSpace -= (TextToEncrypt.Length + Marker.Length * 2) * 4 + AddSignature;
+            //Encode Here -->
+            ConvertedMessage = ConvertStringToBits(ConvertedMessage + "<_T>" + TextToEncrypt + "</T>");
+
+            for (int y = 0; y < Image.Height; y++)
+            {
+                for (int x = 0; x < Image.Width; x++)
+                {
+                    System.Drawing.Color PixelColor = Image.GetPixel(x, y);
+
+                    int TempR = PixelColor.R;
+                    int TempG = PixelColor.G;
+                    int TempB = PixelColor.B;
+
+                    if (ColoredImage)
+                    {
+                        for (int channel = 0; channel < 3; channel++)
+                        {
+                            switch (channel)
+                            {
+                                case 0:
+                                    if (ConvertedMessage.Length > 1)
+                                    {
+                                        TempR = (TempR >> 2) << 2;
+                                        TempR += Convert.ToInt32((ConvertedMessage[0].ToString() + ConvertedMessage[1].ToString()), 2);
+                                        ConvertedMessage = ConvertedMessage.Remove(0, 2);
+                                    }
+                                    break;
+                                case 1:
+                                    if (ConvertedMessage.Length > 1)
+                                    {
+                                        TempG = (TempG >> 2) << 2;
+                                        TempG += Convert.ToInt32((ConvertedMessage[0].ToString() + ConvertedMessage[1].ToString()), 2);
+                                        ConvertedMessage = ConvertedMessage.Remove(0, 2);
+                                    }
+                                    break;
+                                case 2:
+                                    if (ConvertedMessage.Length > 1)
+                                    {
+                                        TempB = (TempB >> 2) << 2;
+                                        TempB += Convert.ToInt32((ConvertedMessage[0].ToString() + ConvertedMessage[1].ToString()), 2);
+                                        ConvertedMessage = ConvertedMessage.Remove(0, 2);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int TempVal = PixelColor.R;
+                        if (ConvertedMessage.Length > 0)
+                        {
+                            TempVal = (TempVal >> 2) << 2;
+                            TempVal += Convert.ToInt32((ConvertedMessage[0].ToString() + ConvertedMessage[1].ToString()), 2);
+                            ConvertedMessage = ConvertedMessage.Remove(0, 2);
+                        }
+
+                        TempR = TempVal;
+                        TempG = TempVal;
+                        TempB = TempVal;
+                    }
+
+                    Image.SetPixel(x, y, Color.FromArgb(TempR, TempG, TempB));
+
+                    if (ConvertedMessage.Length == 0)
+                    {
+                        ElementLine = y;
+                        ElementColumn = x;
+                        return true;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool EncryptGrayImage(Bitmap ImageToEncrypt, string Code)
+        {
+            int AddSignature = 0;
+            if (ElementLine == 0 && ElementColumn == 0)
+            {
+                AddSignature = Signature.Length * 4; // 4 = Pixels to encrypt one character
+            }
+
+            if (ColoredImage)
+            {
+
+            }
+            else
+            {
+
+            }
+            return true;
+        }
+
+        public bool EncryptRGBImage(Bitmap ImageToEncrypt, string Code)
+        {
+            int AddSignature = 0;
+            if (ElementLine == 0 && ElementColumn == 0)
+            {
+                AddSignature = Signature.Length * 4; // 4 = Pixels to encrypt one character
+            }
+
+            if (ColoredImage)
+            {
+
+            }
+            else
+            {
+
+            }
+            return true;
+        }
+
+        public bool DecryptText(ref string DecryptedText, string Code)
+        {
+            string BitArray = "";
+            for (int y = 0; y < Image.Height; y++)
+            {
+                for (int x = 0; x < Image.Width; x++)
+                {
+                    System.Drawing.Color PixelColor = Image.GetPixel(x, y);
+                    if (ColoredImage)
+                    {
+                        BitArray += (Convert.ToString(PixelColor.R, 2).PadLeft(8, '0')).Remove(0, 6);
+                        BitArray += (Convert.ToString(PixelColor.G, 2).PadLeft(8, '0')).Remove(0, 6);
+                        BitArray += (Convert.ToString(PixelColor.B, 2).PadLeft(8, '0')).Remove(0, 6);
+                    }
+                    else
+                    {
+                        BitArray += (Convert.ToString(PixelColor.R, 2).PadLeft(8, '0')).Remove(0, 6);
+                    }
+                    if (BitArray.Length >= 8)
+                    {
+                        DecryptedText += ConvertBitsToChar(BitArray.Substring(0, 8));
+                        BitArray = BitArray.Remove(0, 8);
+                    }
+                }
+            }
+            return true;
+        }
+
+        private string ConvertStringToBits(string TextToConvert)
+        {
+            string result = "";
+            foreach (char letter in TextToConvert)
+            {
+                result += Convert.ToString((int)letter, 2).PadLeft(9, '0');
+            }
+            return result;
+        }
+
+        private char ConvertBitsToChar(string BitArray)
+        {
+            int Power = 1;
+            int ResultedNumber = 0;
+
+            char[] charArray = BitArray.ToCharArray();
+            Array.Reverse(charArray);
+            BitArray = new string(charArray);
+
+            foreach (char Letter in BitArray)
+            {
+                if (Letter == '1')
+                {
+                    ResultedNumber += Power;
+                }
+                Power *= 2;
+            }
+            return (char)ResultedNumber;
+        }
+    }
+}
---
+++ Steganography Encryption/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Steganography_Encryption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //First Tab
+        bool textBox_TextInput = false;
+        bool textBox_ImageInput = false;
+
+        bool ImageLoaded_Encryption = false;
+        string ImageFormat = "";
+        System.Drawing.Bitmap ImageToEncrypt = null;
+
+        //Second Tab
+        System.Drawing.Bitmap ImageToDecode = null;
+        bool ImageLoaded_Decryption = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabEncryptClick_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Height = 600;
+            Application.Current.MainWindow.Width = 902.5;
+        }
+
+        private void TabDecryptClick_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Height = 600;
+            Application.Current.MainWindow.Width = 1300;
+            tabControl.Width = 1263;
+        }
+
+        private void TabPassGenClick_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Height = 600;
+            Application.Current.MainWindow.Width = 800;
+        }
+
+        private void buttonLoadImage_MouseClick(object sender, RoutedEventArgs e)
+        {
+            //Load Image to Encrypt --> 1st Tab
+            OpenFileDialog selectImageForEncryption = new OpenFileDialog();
+
+            selectImageForEncryption.Multiselect = false;
+            selectImageForEncryption.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            selectImageForEncryption.ShowDialog();
+
+            if (selectImageForEncryption.FileName.Length > 0)
+            {
+                ImageToEncrypt = new Bitmap(System.Drawing.Image.FromFile(selectImageForEncryption.FileName));
+                ImageBox_imageEncrypt.Source = ToBitmapImage(ImageToEncrypt);
+                ImageFormat = selectImageForEncryption.FileName.Substring(selectImageForEncryption.FileName.Length - 3); //Last 3 characters == jpg, png ...
+                ImageLoaded_Encryption = true;
+            }
+        }
+
+        private void buttonSaveImage_MouseClick(object sender, RoutedEventArgs e)
+        {
+            //Save Encrypted Image --> 1st Tab
+            if (ImageLoaded_Encryption != false)
+            {
+                SaveFileDialog saveImageDialog = new SaveFileDialog();
+
+                saveImageDialog.Filter = "Bitmap Image|*.bmp";// Jpeg Image|*.jpg|PNG Image|*.png";
+                saveImageDialog.Title = "Save Encrypted Image";
+                saveImageDialog.FileName = "encrypted_image";
+                saveImageDialog.ShowDialog();
+
+                switch (ImageFormat)
+                {
+                    case "bmp":
+                        ImageToEncrypt.Save(saveImageDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    //case "png":
+                    //    ImageToEncrypt.Save(saveImageDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    //    break;
+                    //case "jpg":
+                    //    ImageToEncrypt.Save(saveImageDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    //    break;
+                    default:
+                        MessageBox.Show("Picutre could not be saved!", "Error", MessageBoxButton.OK);
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is no image loaded!", "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void textBox_TextToEncrypt_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            textBox_TextInput = true;
+            textBox_TextToEncrypt.Clear();
+            textBox_TextToEncrypt.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void textBox_ImageToEncrypt_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            textBox_ImageInput = true;
+            textBox_ImageToEncrypt.Clear();
+            textBox_ImageToEncrypt.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void buttonEncryptData_Click(object sender, RoutedEventArgs e)
+        {
+            //EncryptData Button --> 1st Tab
+
+            //De verificat partea cu codul!
+
+            if (textBox_TextInput || textBox_ImageInput)
+            {
+                Classes.EncryptImage_LastTwoBits ApplyEncryption = new Classes.EncryptImage_LastTwoBits(ref ImageToEncrypt, !isGrayScale(ImageToEncrypt));
+                if (textBox_TextInput && textBox_TextToEncrypt.Text.Length > 0)
+                {
+                    if (!ApplyEncryption.EncryptText(textBox_TextToEncrypt.Text, "cod"))
+                    {
+                        MessageBox.Show("Encryption Failed!", "Error", MessageBoxButton.OK);
+                        return;
+                    }
+                    ImageBox_imageEncrypt.Source = ToBitmapImage(ImageToEncrypt);
+                    MessageBox.Show("Encryption Completed!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is nothing to Encrypt", "Error!", MessageBoxButton.OK);
+            }
+        }
+
+        private static Boolean isGrayScale(Bitmap img)
+        {
+            for (Int32 h = 0; h < img.Height; h++)
+                for (Int32 w = 0; w < img.Width; w++)
+                {
+                    System.Drawing.Color color = img.GetPixel(w, h);
+                    if ((color.R != color.G || color.G != color.B || color.R != color.B) && color.A != 0)
+                    {
+                        return false;
+                    }
+                }
+            return true;
+        }
+
+        public static BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        private void button_LoadEncryptedImage_Click(object sender, RoutedEventArgs e)
+        {
+            //Load Encrypted Image --> 2nd Tab
+            OpenFileDialog selectEncryptedImage = new OpenFileDialog();
+
+            selectEncryptedImage.Multiselect = false;
+            selectEncryptedImage.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            selectEncryptedImage.ShowDialog();
+
+            if (selectEncryptedImage.FileName.Length > 0)
+            {
+                ImageToDecode = new Bitmap(System.Drawing.Image.FromFile(selectEncryptedImage.FileName));
+                ImageBox_EncodedImage.Source = ToBitmapImage(ImageToDecode);
+                ImageLoaded_Decryption = true;
+            }
+        }
+
+        private void buttonDecryptData_Click(object sender, RoutedEventArgs e)
+        {
+            //DecryptData Button --> 2nd Tab
+
+            //Nu uita sa aplici codul!
+
+            if (ImageLoaded_Decryption)
+            {
+                Classes.EncryptImage_LastTwoBits ApplyDecryption = new Classes.EncryptImage_LastTwoBits(ref ImageToDecode, !isGrayScale(ImageToDecode));
+                string ResultedText = "";
+                ApplyDecryption.DecryptText(ref ResultedText, "cod");
+                textBox_DecryptedText.Text = ResultedText;
+                MessageBox.Show("Decryption Completed!");
+            }
+            else
+            {
+                MessageBox.Show("There is nothing to Decrypt", "Error!", MessageBoxButton.OK);
+            }
+        }
+    }
+}
